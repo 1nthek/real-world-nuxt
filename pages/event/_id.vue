a8029a0a37af82c74cc6bce7d67f80023367de87
IN DUCK KANG <idk0919@naver.com>
2021-01-25 15:14:40 +0900

    feat: page route and error page

---
+++ pages/event/_id.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Event #{{ id }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  head() {
+    return {
+      title: 'Event #' + this.id,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'What you need to know about event #' + this.id,
+        },
+      ],
+    }
+  },
+  computed: {
+    id() {
+      return this.$route.params.id
+    },
+  },
+})
+</script>
---
+++ pages/event/create.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <div>
+      <h1>Create an Events</h1>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  head() {
+    return {
+      title: 'Create an Event',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'You can create a new event in your neighborhood',
+        },
+      ],
+    }
+  },
+})
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
